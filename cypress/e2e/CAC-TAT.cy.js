cec78adf2bd629eadd0de96b2832b6e9627c2c98
Tiago Bernardes <tiagobernard@iMac-de-Tiago.home>
2025-10-15 13:56:26 -0300

    test: Realizados testes das lições 1 e 2.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -13,15 +13,15 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#firstName').type('Tiago')
     cy.get('#lastName').type('Gomes')
     cy.get('#email').type('tiago@tiagobernardes.com.br')
-    cy.get('#open-text-area').type(longText, {delay: 0})
-    cy.get('.button[type="submit"]').click()
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.contains('button', 'Enviar').click()
     cy.get('.success').should('be.visible')
   })
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
     cy.get('#firstName').type('Tiago')
     cy.get('#lastName').type('Gomes')
-    cy.get('#email').type('tiago@tiagobernardes,com.br')
-    cy.get('.button[type="submit"]').click()
+    cy.get('#email').type('tiago@tiagobernardes.com.br')
+    cy.contains('button', 'Enviar').click()
     cy.get('.error').should('be.visible')
   })
   it('campo telefone continua vazio quando preencido com um valor não-numérico', () => {
@@ -29,14 +29,47 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .type('abcde')
       .should('have.value', '')
   })
-  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
     cy.get('#firstName').type('Tiago')
     cy.get('#lastName').type('Gomes')
     cy.get('#email').type('tiago@tiagobernardes.com.br')
     cy.get('#open-text-area').type('Teste!!!')
     cy.get('#phone-checkbox').click()
-    cy.get('.button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
+  })
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Tiago')
+      .should('have.value', 'Tiago')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type('Bernardes')
+      .should('have.value', 'Bernardes')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type('tiago@tiagobernardes.com.br')
+      .should('have.value', 'tiago@tiagobernardes.com.br')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#phone')
+      .type('31999999999')
+      .should('have.value', '31999999999')
+      .clear()
+      .should('have.value', '')
+  })
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+  })
+
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+    cy.get('.success').should('be.visible')
   })
 })
